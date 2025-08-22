4b0b265e0f729874959dc900410ec6b82d5218df
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-08-22 16:23:21 +0800

    FEATURE: Add additional pluralization tests for words ending with sh, ch, x, and z

--- spec/string_extensions_spec.rb
+++ spec/string_extensions_spec.rb
@@ -35,5 +35,21 @@ RSpec.describe 'String Extensions' do
       expect('users'.pluralize).to eq('users')
       expect('posts'.pluralize).to eq('posts')
     end
+
+    it 'adds es for words ending with sh' do
+      expect('brush'.pluralize).to eq('brushes')
+    end
+
+    it 'adds es for words ending with ch' do
+      expect('church'.pluralize).to eq('churches')
+    end
+
+    it 'adds es for words ending with x' do
+      expect('box'.pluralize).to eq('boxes')
+    end
+
+    it 'adds es for words ending with z' do
+      expect('quiz'.pluralize).to eq('quizzes')
+    end
   end
 end
