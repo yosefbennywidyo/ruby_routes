a02544bad0dc3e11ab843c95951d850acc5c6f98
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-08-30 11:23:42 +0800

    Refactor: simplify error handling in Router::Builder#build method

--- lib/ruby_routes/router/builder.rb
+++ lib/ruby_routes/router/builder.rb
@@ -53,12 +53,7 @@ module RubyRoutes
         allowed_methods = RubyRoutes::Constant::RECORDED_METHODS
 
         recorded_calls.each do |(method_name, arguments, definition_block)|
-          if method_name.is_a?(Symbol) && allowed_methods.include?(method_name)
-            router.public_send(method_name, arguments, definition_block)
-          else
-            # Either not a Symbol or not in our allowlist - reject with clear error
-            raise ArgumentError, "Invalid router method: #{method_name.inspect}"
-          end
+          raise ArgumentError, "Invalid router method: #{method_name.inspect}" unless allowed_methods.include?(method_name)
         end
         router.finalize!
         router
