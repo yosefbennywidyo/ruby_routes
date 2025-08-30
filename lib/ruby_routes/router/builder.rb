7e5281e94727afa09debb27ac83f49b157064ada
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-08-30 10:40:16 +0800

    Restore builder.rb to state from bd05bf8

--- lib/ruby_routes/router/builder.rb
+++ lib/ruby_routes/router/builder.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require_relative '../constant'
 module RubyRoutes
   class Router
@@ -38,8 +37,6 @@ module RubyRoutes
 
       # ---- DSL Recording -------------------------------------------------
       RubyRoutes::Constant::RECORDED_METHODS.each do |method_name|
-        next if %i[build initialize recorded_calls].include?(method_name)
-
         define_method(method_name) do |*arguments, &definition_block|
           @recorded_calls << [method_name, arguments, definition_block]
           nil
@@ -51,34 +48,9 @@ module RubyRoutes
       # @return [RubyRoutes::Router] finalized router
       def build
         router = Router.new
-        allowlist = RubyRoutes::Constant::RECORDED_METHODS
-
         recorded_calls.each do |(method_name, arguments, definition_block)|
-          # Security: only allow predefined DSL methods (prevents arbitrary method execution)
-          raise ArgumentError, "Disallowed DSL method: #{method_name.inspect}" unless allowlist.include?(method_name)
-
-          # (Optional) shallow dup args to avoid later mutation side‑effects
-          safe_args = arguments.map do |argument|
-            argument.is_a?(String) || argument.is_a?(Numeric) || argument.is_a?(Symbol) ? argument : argument.dup
-          rescue StandardError
-            argument
-          end
-
-          case method_name
-          when :get, :post, :put, :patch, :delete, :head, :options
-            router.send(method_name, *safe_args, &definition_block)
-          when :match, :root, :resources, :resource
-            router.send(method_name, *safe_args, &definition_block)
-          when :namespace, :scope, :constraints, :defaults
-            router.send(method_name, *safe_args, &definition_block)
-          when :mount, :concern, :concerns
-            router.send(method_name, *safe_args, &definition_block)
-          else
-            # This shouldn't be reached due to allowlist check above
-            raise ArgumentError, "Unsupported DSL method: #{method_name.inspect}"
-          end
+          router.public_send(method_name, *arguments, &definition_block)
         end
-
         router.finalize!
         router
       end
